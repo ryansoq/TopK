8da1a4511944e1d8d544f79a48b061d720a36ee6
Yuan-Ming Chang <ymchang@andestech.com>
2021-09-30 18:07:39 +0800

    Adjust file content

--- main.cc
+++ main.cc
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     Tool tool;
     int8_t predictions[DATA_SIZE] = {63, 77, -88, 30, 56, }; // e.g. by some inference
     auto depredictions = tool.decode_predictions<int8_t, DATA_SIZE>(predictions, top = 5, "imagenet1000_clsidx_to_labels.txt");
+
     auto [index, label, value] = depredictions[0];
     std::cout << "The image prediction Top 1 index is: " << index << ", label name: " << label << ", confidence: " << (float)value << std::endl;
 
